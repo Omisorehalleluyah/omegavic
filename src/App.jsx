fa3e9257cfd54b87fb3689467da716ee2fbbd56c
Omisore Halleluyah Oluwatobiloba <108987108+Omisorehalleluyah@users.noreply.github.com>
2024-01-15 22:50:40 +0000

    set hompage to github deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Omisore halleluyah",
+  title: "Data anlyst & Content Creator",
+  email: "shallyquick.com",
+  gitHub: "Omisorehalleluyah",
+  instagram: "shallyquick",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Hallelu2omisore",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
